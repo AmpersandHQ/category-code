4a00e5d03abde6008981009c6292fc6267649d09
Daniel Doyle <tr33m4n@googlemail.com>
2019-06-19 14:40:30 +0100

    Fix static analysis issues

--- Model/CodedCategoryProductLink.php
+++ Model/CodedCategoryProductLink.php
@@ -8,9 +8,12 @@ class CodedCategoryProductLink extends AbstractExtensibleModel implements CodedC
 {
     public function getSku()
     {
-       return $this->_getData(self::SKU);
+        return $this->_getData(self::SKU);
     }
 
+    /**
+     * @return \Ampersand\CategoryCode\Api\Data\CodedCategoryProductLinkInterface
+     */
     public function setSku($sku)
     {
         return $this->setData(self::SKU, $sku);
@@ -21,6 +24,9 @@ class CodedCategoryProductLink extends AbstractExtensibleModel implements CodedC
         return $this->_getData(self::POSITION);
     }
 
+    /**
+     * @return \Ampersand\CategoryCode\Api\Data\CodedCategoryProductLinkInterface
+     */
     public function setPosition($position)
     {
         return $this->setData(self::POSITION, $position);
@@ -31,6 +37,9 @@ class CodedCategoryProductLink extends AbstractExtensibleModel implements CodedC
         return $this->_getData(self::CATEGORY_CODE);
     }
 
+    /**
+     * @return \Ampersand\CategoryCode\Api\Data\CodedCategoryProductLinkInterface
+     */
     public function setCategoryCode($categoryCode)
     {
         return $this->setData(self::CATEGORY_CODE, $categoryCode);
@@ -41,6 +50,9 @@ class CodedCategoryProductLink extends AbstractExtensibleModel implements CodedC
         return $this->_getExtensionAttributes();
     }
 
+    /**
+     * @return \Ampersand\CategoryCode\Api\Data\CodedCategoryProductLinkInterface
+     */
     public function setExtensionAttributes(\Ampersand\CategoryCode\Api\Data\CodedCategoryProductLinkExtensionInterface $extensionAttributes)
     {
         return $this->_setExtensionAttributes($extensionAttributes);
